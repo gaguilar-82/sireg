ddfe390cb33089c87051dc6a152eafc73adaa52a
Gerardo Aguilar <aguilarsamano@gmail.com>
2021-09-15 09:41:20 -0500

    Dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Colonia;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('admin.index');
+      $invisur = Colonia::where('TipoColonia','=', 'PATRIMONIO INVISUR')->count();
+      $crett = Colonia::where('TipoColonia','=', 'PATRIMONIO CRETT')->count();
+      $barrios = Colonia::where('TipoColonia','=', 'BARRIOS HISTÓRICOS')->count();
+      $donacion = Colonia::where('TipoColonia','=', 'DONACIÓN CONDICIONAL')->count();
+      $veladero = Colonia::where('TipoColonia','=', 'PARQUE NACIONAL EL VELADERO')->count();
+
+        return view('admin.index', compact('invisur','crett','barrios','donacion','veladero'));
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,11 +3,42 @@
 @section('title', 'Inicio')
 
 @section('content_header')
-    <h1>SIREG</h1>
+    <h1>Dashboard</h1>
 @stop
 
 @section('content')
-    <p>Bienvenido a SIREG.</p>
+  <div class="row">
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header">
+          Número de Lotes por Tipo de Colonia
+        </div>
+        <div class="card-body">
+            <canvas id="chart" width="300" height="300"></canvas>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header">
+          Número de Escrituras por Tipo de Colonia
+        </div>
+        <div class="card-body">
+          <canvas id="chart" width="300" height="300"></canvas>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card">
+        <div class="card-header">
+          Ingresos Totales por Concepto
+        </div>
+        <div class="card-body">
+          <canvas id="chart" width="300" height="300"></canvas>
+        </div>
+      </div>
+    </div>
+  </div>
 @stop
 
 @section('css')
@@ -20,4 +51,34 @@
     @if (session('eliminar') == 'ok')
         <script type="text/javascript" src="{{ asset('js/eliminado.js') }}"></script>
     @endif
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+      var ctx = document.getElementById('chart');
+      var invisur=@json($invisur);
+      var crett=@json($crett);
+      var barrios=@json($barrios);
+      var donacion=@json($donacion);
+      var veladero=@json($veladero);
+      var myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Patrimonio INVISUR', 'Patrimonio CRETT', 'Barrios Históricos', 'Donación Condicional', 'Parque Nacional El Veladero'],
+            datasets: [{
+                label: '# of Votes',
+                data: [invisur, crett, barrios, donacion, veladero],
+                backgroundColor: [
+                    'rgba(51, 102, 255, 0.2)',
+                    'rgba(0, 153, 255, 0.2)',
+                    'rgba(255, 153, 51, 0.2)',
+                    'rgba(153, 255, 51, 0.2)',
+                    'rgba(102, 0, 255, 0.2)'
+                ],
+            }]
+        },
+        options: {
+          responsive: false
+        }
+    });
+</script>
 @stop
